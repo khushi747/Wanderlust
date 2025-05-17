ada9bb0958957beb9ff9c796c72967a5da85d4d4
Khushi Shukla <khushishukla2323@gmail.com>
2025-05-17 09:42:53 +0530

    Initial Commit Setup

--- app.js
+++ app.js
@@ -18,9 +18,30 @@ main()
     console.log("Error connecting to MongoDB", err);
   });
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.send("Hello World from root");
 });
+
+app.get("/listings", async (req, res) => {
+  const allListings=await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
+
+// app.get("/testListings", async (req, res) => {
+//   let sampleListing = new listing({
+//     title: "Sample Listing",
+//     description: "This is a sample listing",
+//     price: 100,
+//     location: "Sample Location",
+//     country: "Sample Country",
+//   });
+//   await sampleListing.save();
+//   console.log("Sample listing saved to database");
+//   res.send("Sample listing saved to database");
+// });
 
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
