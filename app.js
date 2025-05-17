150919448b67057a29cbfb81fbb220e2b4afdc39
Khushi Shukla <khushishukla2323@gmail.com>
2025-05-17 12:04:22 +0530

    added create route

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ main()
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true })); // allows us to parse data from request body
+app.use(express.urlencoded({ extended: true })); // allows us to parse data from request body or url
 
 app.get("/", (req, res) => {
   res.send("Hello World from root");
@@ -31,6 +31,18 @@ app.get("/listings", async (req, res) => {
   res.render("listings/index.ejs", { allListings }); //listings/index.ejs is converted to views/listings/index.ejs using the path set above
 });
 
+//CREATE ROUTE
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings"); 
+});
+
+//SHOW ROUTE
 app.get("/listings/:id", async (req, res) => {
   const { id } = req.params;
   const listing = await Listing.findById(id);
