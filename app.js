e2b76ae5bff4d19c5cdbba34a9c95f99e708efba
Khushi Shukla <khushishukla2323@gmail.com>
2025-05-17 09:30:58 +0530

    connected to Mongodb

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+ 
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+async function main() {
+  console.log("Trying to connect to MongoDB...");
+  await mongoose.connect(MONGO_URL);
+}
 
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+ 
 app.get("/", (req, res) => {
   res.send("Hello World from root");
 });
