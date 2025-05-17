5858abcf8654c0535ead98cdd1778fc8fb71573f
Khushi Shukla <khushishukla2323@gmail.com>
2025-05-17 12:39:44 +0530

    added edit listing route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const Listing = require("./Models/listing");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
@@ -21,6 +22,7 @@ main()
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true })); // allows us to parse data from request body or url
+app.use(methodOverride("_method")); // allows us to use PUT and DELETE requests
 
 app.get("/", (req, res) => {
   res.send("Hello World from root");
@@ -39,7 +41,7 @@ app.get("/listings/new", (req, res) => {
 app.post("/listings", async (req, res) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
-  res.redirect("/listings"); 
+  res.redirect("/listings");
 });
 
 //SHOW ROUTE
@@ -47,6 +49,19 @@ app.get("/listings/:id", async (req, res) => {
   const { id } = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs", { listing });
+});
+
+//EDIT ROUTE
+app.get("/listings/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+app.put("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${listing._id}`);
 });
 
 app.listen(8080, () => {
