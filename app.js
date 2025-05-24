e5f5bf4044aea3b1ef00ca320d5a54f9c7fe71ad
Khushi Shukla <khushishukla2323@gmail.com>
2025-05-24 23:22:30 +0530

    modified session settings

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ const sessionOptions = {
   secret: "mysecretsupercode",
   resave: false,
   saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly : true, // set to true if using HTTPS
+  },
 };
 
 app.use(session(sessionOptions));
